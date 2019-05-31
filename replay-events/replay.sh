f236a6cabd80d47565cd8c6b28acee5a084cccc8
Alexander Gee <alexander.gee@digital.cabinet-office.gov.uk>
2019-05-31 13:23:52 +0100

    Add helpers for replaying events into Matomo
    The provided Dockerfile uses a clone of a fork of the Matomo
    analytics script, capable of handling `X-Forwarded-For` headers.
    This, and a pre-defined access log are copied into the image.
    A template command to replay events is echoed into the terminal -
    a Matomo password currently has to be entered in plaintext, so should
    not be baked into the Docker image.
    The container is then spun up for the developer to enter the command.
    
    This setup is clearly not ideal. But it is functional.
    On the basis that something is better than nothing, it should be
    iterated on as improvements become clearer.

---
+++ replay-events/replay.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [ ! -f "access.log" ]; then
+    echo "Please copy the 'access.log' you wish to replay into this directory."
+    exit 1
+fi
+
+docker build -t matomo-import .
+
+cat << 'EOF'
+In the container, you will want to run a command like:
+python -u /log-analytics/import_logs.py \
+    --url=<matomo-url> \
+    --log-format-name=nginx_json \
+    --replay-tracking \
+    --enable-static \
+    --enable-bots \
+    --enable-reverse-dns \
+    --recorders=2 \
+    --login=<matomo-login> \
+    --password='<matomo-password>' \
+    /access.log
+Remember to substitute in your Matomo URL and login credentials.
+EOF
+
+docker run --rm -it matomo-import
